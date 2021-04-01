8dd6f210a1a11dbf94ca4e92f5f6878e01b34245
sakurai-ryo <sakurairyo.10do@gmail.com>
2021-04-02 01:29:35 +0900

    update: change slice roop

--- save/save.go
+++ save/save.go
@@ -4,9 +4,7 @@ import (
 	"aggregate_errors/shared"
 	"aggregate_errors/typefile"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
 )
 
 var env string
@@ -21,49 +19,22 @@ const (
 	Fixed       = "Fixed"
 )
 
+var S = []string{"Environment", "Description", "Solution", "Fixed"}
+
 // Save ... save error and context interactively
 func Save() error {
-	// pipeStr, err := readPipe()
-	// if err != nil {
-	// 	return err
-	// }
-	// log.Println("Error: ", pipeStr)
-
 	builder := typefile.NewAggregated("")
 
-	if err := build("Environment", &builder); err != nil {
-		return err
+	for _, s := range S {
+		if err := build(s, &builder); err != nil {
+			return err
+		}
 	}
-	if err := build("Description", &builder); err != nil {
-		return err
-	}
-	if err := build("Solution", &builder); err != nil {
-		return err
-	}
-	if err := build("Fixed", &builder); err != nil {
-		return err
-	}
+
 	result := builder.Build()
 	log.Print(result)
 
 	return nil
-}
-
-func readPipe() (string, error) {
-	read, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		return "", nil
-	}
-	return string(read), nil
-}
-
-func readPrompt(label string) (string, error) {
-	prompt := shared.NewPromptui(label)
-	input, err := prompt.Run()
-	if err != nil {
-		return "", err
-	}
-	return input, nil
 }
 
 func build(label string, builder *typefile.AggregateBulder) error {
@@ -89,4 +60,13 @@ func build(label string, builder *typefile.AggregateBulder) error {
 		return nil
 	}
 	return fmt.Errorf("Internal ServerError: Label not found")
+}
+
+func readPrompt(label string) (string, error) {
+	prompt := shared.NewPromptui(label)
+	input, err := prompt.Run()
+	if err != nil {
+		return "", err
+	}
+	return input, nil
 }
